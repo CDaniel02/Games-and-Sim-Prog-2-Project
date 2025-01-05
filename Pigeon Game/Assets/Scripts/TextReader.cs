5f016de50d79eeb509870eced26c57d4d6cffa24
elispearscsu <elispears123@gmail.com>
2025-01-05 12:01:20 -0500

    started to add text reader and letters.txt

---
+++ Pigeon Game/Assets/Scripts/TextReader.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Internal;
+
+public class JSONReader : MonoBehaviour
+{
+	private string _filepath = "letters.txt";
+
+    private void Start()
+    {
+        
+
+    }
+
+    private void Read()
+    {
+        if (File.Exists(file))
+        {
+            // Store each line in array of strings 
+            string[] lines = File.ReadAllLines(_filepath);
+
+            string currentLine = lines[0];
+
+            if(currentLine == "start")
+            {
+                while(currentLine != "end")
+                {
+
+                }
+            }
+
+            foreach (string ln in lines)
+                Console.WriteLine(ln);
+        }
+
+
+    }
+}
+
