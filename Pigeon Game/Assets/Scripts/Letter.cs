264c501da6a59b82dc51ebec5a8be9edd2f3a4ba
elispearscsu <elispears123@gmail.com>
2025-01-05 11:04:19 -0500

    Update Letter.cs

--- Pigeon Game/Assets/Scripts/Letter.cs
+++ Pigeon Game/Assets/Scripts/Letter.cs
@@ -27,7 +27,11 @@ public class Letter
 	{
 		To = to;
 		From = from;
-		Body = body; 
+		Body = body;
+
+		ToReponse = "";
+		FromReponse = "";
+		PrereqLetters = new List<Letter>(); 
 	}
 	public Letter(string to, string from) : this(to, from, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.") { }
 	public Letter() : this("NO NAME", "NO NAME") { }
