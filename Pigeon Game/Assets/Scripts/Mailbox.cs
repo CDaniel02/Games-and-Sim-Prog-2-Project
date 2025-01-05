af6c0ef57f665fda994cfd9ca25ae749c21cf8ed
elispearscsu <elispears123@gmail.com>
2025-01-05 15:47:56 -0500

    RETURN LETTERS STILL NOTWORKING IM ABOUT TO KMS

--- Pigeon Game/Assets/Scripts/Mailbox.cs
+++ Pigeon Game/Assets/Scripts/Mailbox.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEngine; 
 
 public class Mailbox
 {
 	private List<Letter> _incomingMailbox;
     private List<Letter> _outgoingMailbox;
-	//public Dictionary<string, List<Letter>> OutgoingMailbox { get { return _outgoingMailbox; } }
 
     public Mailbox(List<Letter> fromMailBox)
 	{
@@ -16,6 +16,7 @@ public class Mailbox
 
 	public void AddIncomingMail(Letter letter)
 	{
+		Debug.Log("Incoming Mail added to mailbox");
 		_incomingMailbox.Add(letter);
 		ValidateLetters(letter); 
 	}
@@ -27,21 +28,18 @@ public class Mailbox
 
     private void ValidateLetters(Letter givenLetter)
 	{
-		//foreach(KeyValuePair<string, List<Letter>> letters in _outgoingMailbox)
-		//{
+		Debug.Log("Validate letters function called"); 
 		foreach(Letter letter in _outgoingMailbox)
 		{
-			letter.ValidatePrereqs(givenLetter); 
+			letter.ValidatePrereqs(givenLetter);
+			Debug.Log("Letter has validated letters"); 
 		}
-		//}
 	}
 
 	public List<Letter> GetOutgoingMail()
 	{
 		List<Letter> toReturn = new List<Letter>();
 
-        //foreach (KeyValuePair<string, List<Letter>> letters in _outgoingMailbox)
-        //{
         foreach (Letter letter in _outgoingMailbox)
         {
             if (letter.ValidatePrereqs())
@@ -49,35 +47,9 @@ public class Mailbox
                 toReturn.Add(letter);
             }
         }
-        //}
 
         foreach (Letter letter in toReturn) _outgoingMailbox.Remove(letter);
         return toReturn;
     }
-
-    //private void RemoveFromOutGoingMail(Letter letter)
-    //{
-    //    _outgoingMailbox.Remove(letter); 
-    //}
-
-    /*
-	public int CountOfOutgoingMail()
-	{
-        int count = 0;
-
-        foreach (KeyValuePair<string, List<Letter>> letters in _outgoingMailbox)
-        {
-            foreach (Letter letter in letters.Value)
-            {
-                if (letter.ValidatePrereqs())
-                {
-                    count++; 
-                }
-            }
-        }
-
-        return count;
-    }
-    */ 
 }
 
