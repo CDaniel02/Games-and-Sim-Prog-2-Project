11f2b38066bdd87693970cd40cf77c4977c009b6
CDaniel02 <daniel_chancellor@students.columbusstate.edu>
2025-01-23 23:29:52 -0500

    creation of nav mesh and got npc movement working finally

--- Pigeon Game/Assets/Scripts/NPC/Core/NPC.cs
+++ Pigeon Game/Assets/Scripts/NPC/Core/NPC.cs
@@ -1,16 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 // renamed to just NPC NPC controller or something like that 
 
 public class NPC : MonoBehaviour
 {
     // name field that gets pulled from the hierarchuy
-
-    
     public string Name;
 
     /*
@@ -26,12 +24,18 @@ public class NPC : MonoBehaviour
     private Queue<string> _dialogQueue;
 
     private bool pigeonNearby = false;
-    public Vector3 npcSpawnPoint;
-    public Vector3 npcMovementEndPoint;
-    public float walkingDuration = 3.0f;
+    public NavMeshAgent agent;
+    public float range; //radius of sphere
+    public Animator animator;
 
-    public void Start()
+    public Transform centrePoint; //centre of the area the agent wants to move around in
+    //instead of centrePoint you can set it as the transform of the agent if you don't care about a specific area
+
+    private void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        animator.SetBool("idle", true);
         // ToMailbox = new Dictionary<string, Letter>();
         mailbox = new Mailbox(); 
         Name = GetComponent<CapsuleCollider>().name;
@@ -56,21 +60,44 @@ public class NPC : MonoBehaviour
         // ToMailbox[testLetter.To] = testLetter;
 
         _dialogQueue = new Queue<string>();
-        StartCoroutine(npcMove());
+
     }
 
-    private IEnumerator npcMove()
+    private void Update()
     {
-          while (pigeonNearby == false)
+        if (pigeonNearby)
         {
-            yield return null;
-            //StartCoroutine(npcMoveToPoint(npcSpawnPoint, npcMovementEndpoint));
-            //StartCoroutine(npcMoveToPoint(npcMovementEndpoint, npcSpawnPoint));
+            Debug.Log("Pigeon is nearby I cant move");
+            return;
+        }
+        if (agent.remainingDistance <= agent.stoppingDistance) //done with path
+        {
+            animator.SetBool("idle", true);
+            Vector3 point;
+            if (RandomPoint(centrePoint.position, range, out point)) //pass in our centre point and radius of area
+            {
+                Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f); //so you can see with gizmos
+                agent.SetDestination(point);
+                animator.SetBool("idle", false);
+            }
         }
     }
-    private IEnumerator npcMoveToPoint()
+
+    private bool RandomPoint(Vector3 center, float range, out Vector3 result)
     {
-       yield return null;
+
+        Vector3 randomPoint = center + Random.insideUnitSphere * range; //random point in a sphere 
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas)) //documentation: https://docs.unity3d.com/ScriptReference/AI.NavMesh.SamplePosition.html
+        {
+            //the 1.0f is the max distance from the random point to a point on the navmesh, might want to increase if range is big
+            //or add a for loop like in the documentation
+            result = hit.position;
+            return true;
+        }
+
+        result = Vector3.zero;
+        return false;
     }
 
     public bool Interact(PlayerStateMachine pigeon)
