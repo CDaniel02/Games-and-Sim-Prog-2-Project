25525a70f7a1fe523683392cbfc29f5bb30876c6
violethues <violetspears123@gmail.com>
2025-04-08 17:22:31 -0400

    Revert "letter changes"
    This reverts commit 4a73536d580832715a5cfd0b1c7d499dbc3246b5.

--- Pigeon Game/Assets/Scripts/Letters/LetterScript.cs
+++ Pigeon Game/Assets/Scripts/Letters/LetterScript.cs
@@ -8,15 +8,7 @@ public class LetterScript : MonoBehaviour, IClickable, IDragable
     public TMP_Text FromTextMesh;
     public TMP_Text BodyTextMesh;
 
-    public RectTransform LetterText;
-    public GameObject LetterPage; 
-
-	public Letter thisLetter;
-
-    public int CharacterLimitBeforeExpanding = 220;
-    public int CharactersPerLine = 23;
-    public float TextHeightIncrease = 20f;
-    public float PageScaleIncrease = 0.10f;
+	public Letter thisLetter; 
 
 	public void SetLetter(Letter letter)
 	{
@@ -24,21 +16,8 @@ public class LetterScript : MonoBehaviour, IClickable, IDragable
 		gameObject.name = thisLetter.LetterId + ""; 
 		ToTextMesh.text = "To: " + thisLetter.To;
 		FromTextMesh.text = "From: " + thisLetter.From;
-		BodyTextMesh.text = thisLetter.Body;
-        AdjustSize(); 
+		BodyTextMesh.text = thisLetter.Body; 
 	}
-
-    public void AdjustSize()
-    {
-        if(thisLetter.Body.Length > CharacterLimitBeforeExpanding)
-        {
-            int lines = (int)Mathf.Ceil((thisLetter.Body.Length - CharacterLimitBeforeExpanding) / CharactersPerLine);
-
-            //LetterText.rect.height = LetterText.rect.height + lines * TextHeightIncrease;
-            //LetterPage.transform.localScale.y = LetterPage.transform.localScale.y + lines * PageScaleIncrease;
-            
-        }
-    }
 
 	public void Click(PlayerStateMachine stateMachine)
 	{
