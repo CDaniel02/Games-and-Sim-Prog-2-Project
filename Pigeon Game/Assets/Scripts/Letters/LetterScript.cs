4a73536d580832715a5cfd0b1c7d499dbc3246b5
elispearscsu <elispears123@gmail.com>
2025-04-08 17:06:46 -0400

    letter changes

--- Pigeon Game/Assets/Scripts/Letters/LetterScript.cs
+++ Pigeon Game/Assets/Scripts/Letters/LetterScript.cs
@@ -8,7 +8,15 @@ public class LetterScript : MonoBehaviour, IClickable, IDragable
     public TMP_Text FromTextMesh;
     public TMP_Text BodyTextMesh;
 
-	public Letter thisLetter; 
+    public RectTransform LetterText;
+    public GameObject LetterPage; 
+
+	public Letter thisLetter;
+
+    public int CharacterLimitBeforeExpanding = 220;
+    public int CharactersPerLine = 23;
+    public float TextHeightIncrease = 20f;
+    public float PageScaleIncrease = 0.10f;
 
 	public void SetLetter(Letter letter)
 	{
@@ -16,8 +24,21 @@ public class LetterScript : MonoBehaviour, IClickable, IDragable
 		gameObject.name = thisLetter.LetterId + ""; 
 		ToTextMesh.text = "To: " + thisLetter.To;
 		FromTextMesh.text = "From: " + thisLetter.From;
-		BodyTextMesh.text = thisLetter.Body; 
+		BodyTextMesh.text = thisLetter.Body;
+        AdjustSize(); 
 	}
+
+    public void AdjustSize()
+    {
+        if(thisLetter.Body.Length > CharacterLimitBeforeExpanding)
+        {
+            int lines = (int)Mathf.Ceil((thisLetter.Body.Length - CharacterLimitBeforeExpanding) / CharactersPerLine);
+
+            //LetterText.rect.height = LetterText.rect.height + lines * TextHeightIncrease;
+            //LetterPage.transform.localScale.y = LetterPage.transform.localScale.y + lines * PageScaleIncrease;
+            
+        }
+    }
 
 	public void Click(PlayerStateMachine stateMachine)
 	{
